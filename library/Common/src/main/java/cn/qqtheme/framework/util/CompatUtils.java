37c5b0ca6109360851afb976f17382797cbc1a2e
liyujiang <liyujiang_tk@yeah.net>
2016-12-21 23:26:57 +0800

    移除未用的方法，以便能在低于23的SDK版本上编译

--- library/Common/src/main/java/cn/qqtheme/framework/util/CompatUtils.java
+++ library/Common/src/main/java/cn/qqtheme/framework/util/CompatUtils.java
@@ -4,13 +4,8 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
-import android.support.annotation.ColorInt;
-import android.support.annotation.ColorRes;
 import android.support.annotation.DrawableRes;
-import android.support.annotation.StringRes;
-import android.support.annotation.StyleRes;
 import android.view.View;
-import android.widget.TextView;
 
 /**
  * 兼容旧版&新版
@@ -30,16 +25,6 @@ public class CompatUtils {
         }
     }
 
-    @TargetApi(Build.VERSION_CODES.M)
-    public static void setTextAppearance(TextView view, @StyleRes int appearanceRes) {
-        if (Build.VERSION.SDK_INT < 23) {
-            //noinspection deprecation
-            view.setTextAppearance(view.getContext(), appearanceRes);
-        } else {
-            view.setTextAppearance(appearanceRes);
-        }
-    }
-
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public static Drawable getDrawable(Context context, @DrawableRes int drawableRes) {
         if (Build.VERSION.SDK_INT < 21) {
@@ -47,27 +32,6 @@ public class CompatUtils {
             return context.getResources().getDrawable(drawableRes);
         } else {
             return context.getDrawable(drawableRes);
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public static String getString(Context context, @StringRes int stringRes) {
-        if (Build.VERSION.SDK_INT < 21) {
-            //noinspection deprecation
-            return context.getResources().getString(stringRes);
-        } else {
-            return context.getString(stringRes);
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    @ColorInt
-    public static int getColor(Context context, @ColorRes int colorRes) {
-        if (Build.VERSION.SDK_INT < 23) {
-            //noinspection deprecation
-            return context.getResources().getColor(colorRes);
-        } else {
-            return context.getResources().getColor(colorRes, null);
         }
     }
 
